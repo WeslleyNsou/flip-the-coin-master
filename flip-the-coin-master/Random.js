d7b14dc22e0b669fae2b19315c40b61fe2c3027e
WeslleyNsou <120567284+WeslleyNsou@users.noreply.github.com>
2025-03-25 17:04:45 -0300

    projeto concluido

--- flip-the-coin-master/Random.js
+++ flip-the-coin-master/Random.js
@@ -4,9 +4,22 @@ Random.addEventListener('click', function(){
     let result = Math.random() < 0.5 ? "Heads" : "Tail"
     document.getElementById('result-txt').innerHTML = result
     if(result === "Heads"){
-        
+        var img = document.getElementById('result-img') 
+
+        var novaimg = document.createElement("img")
+        novaimg.src = "resources/heads.svg"
+
+        img.innerHTML = "";
+        img.appendChild(novaimg)
+
     }else{
-        console.log('> 0.5')
+        var img = document.getElementById('result-img') 
+
+        var novaimg = document.createElement("img")
+        novaimg.src = "resources/tails.svg"
+
+        img.innerHTML = "";
+        img.appendChild(novaimg)
     }
 
 })
